b0132c57c8500c74f70698a1549adb33be301bfe
febreno <felipesugisawa1@gmail.com>
2025-06-26 11:13:44 -0300

    loading sem mais usuários

--- salaais-new/src/pages/users/index.tsx
+++ salaais-new/src/pages/users/index.tsx
@@ -116,7 +116,7 @@ export function UsersPage() {
         />
       )}
 
-      {!isLoadingMore && userListRef.current.length === 0 && userList.length === 0 && (
+      {isLoadingInitial && !isLoadingMore && userListRef.current.length === 0 && userList.length === 0 && (
         <Text text="Sem mais usuários." size={Size.M} center />
       )}
 
